947c94a7e666df704bdda5fb761eb6037738a7e8
root <root@vmi1632944.contaboserver.net>
2024-02-18 06:49:00 +0100

    added RollApp

---
+++ devnet/cgcg_4122991-1/cgcg_4122991-1.js
@@ -0,0 +1,36 @@
+💈 networks.json:
+{
+  "chainId": "cgcg_4122991-1",
+  "chainName": "cgcg_4122991-1",
+  "rpc": "",
+  "rest": "",
+  "bech32Prefix": "ethm",
+  "currencies": [
+    {
+      "displayDenom": "CGG",
+      "baseDenom": "uCGG",
+      "decimals": 18,
+      "baseDenom": "uCGG",                                                                            
+      "decimals": 18,
+      "logo": "/logos/cgcg_4122991-1.png",
+      "type": "main"
+    }
+  ],
+  "coinType": 60,
+  "faucetUrl": "https://discord.com/channels/956961633165529098/1143231362468434022",
+  "website": "",
+  "logo": "/logos/cgcg_4122991-1.png",
+  "ibc": {
+    "hubChannel": "channel-9296",
+    "channel": "channel-0",
+    "timeout": 172800000
+  },
+  "evm": {
+    "chainId": "0x3ee96f",
+    "rpc": ""
+  },
+  "type": "RollApp",
+  "da": "Avail",
+  "description": "",
+  "analytics": true
+}
